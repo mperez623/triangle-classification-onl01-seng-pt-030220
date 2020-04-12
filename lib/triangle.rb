a85dbc9497c2b03dc405f162d30bc75599845c06
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-12 20:19:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,17 +10,23 @@ def initialize (side1, side2, side3)
 end
 
 def kind
-  if (@s1 +@s2) <= @s3 || 
-end
-
-
-
-
-
-
-
-
-
-
-
+  if (@s1 +@s2) <= @s3 || (@s2 + @s3) <= @s1 || (@s3 + @s1) <= @s2 
+    begin
+      raise TraingleError
+      puts error.message
+    end
+    elsif @s1 == @s2 && @s1 == @s3
+      self.kind = :equilateral 
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      self.kind = :isosceles
+    else 
+      self.kind = :scalene
+    end
+  end
+  
+  class TraingleError < StandardError
+    def message
+      puts "not a traingle"
+    end
+  end
 end
